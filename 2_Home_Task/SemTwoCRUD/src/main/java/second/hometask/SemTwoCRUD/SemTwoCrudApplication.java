11adf7b64033fd707a8cdafa50f4cbf549723af4
KseniiaS <myNew@email.com>
2024-02-09 21:11:05 +0300

    second_home_task_v0.0

---
+++ 2_Home_Task/SemTwoCRUD/src/main/java/second/hometask/SemTwoCRUD/SemTwoCrudApplication.java
@@ -0,0 +1,13 @@
+package second.hometask.SemTwoCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemTwoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemTwoCrudApplication.class, args);
+	}
+
+}
---
+++ 2_Home_Task/SemTwoCRUD/src/test/java/second/hometask/SemTwoCRUD/SemTwoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package second.hometask.SemTwoCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemTwoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
