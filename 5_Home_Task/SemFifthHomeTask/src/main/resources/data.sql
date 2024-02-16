b0f44052b9e122ec5d75be7533f873d4942e792a
KseniiaS <myNew@email.com>
2024-02-16 09:14:46 +0300

    fifth_home_task_v0.2

--- 5_Home_Task/SemFifthHomeTask/src/main/resources/data.sql
+++ 5_Home_Task/SemFifthHomeTask/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tasksTABLE (name_task, status) VALUES ('Сделать зарядку', 'задача завершена')
-INSERT INTO tasksTABLE (name_task, status) VALUES ('Почитать почту', 'в процессе')
-INSERT INTO tasksTABLE (name_task, status) VALUES ('Встреча в 11:00', 'начать')
-INSERT INTO tasksTABLE (name_task, status) VALUES ('Подготовиться к выступлению', 'начать')
+INSERT INTO tasksTABLE (name_task, status, local_date, local_time) VALUES ('Сделать зарядку', 'задача завершена', CURRENT_DATE, CURRENT_TIME)
+INSERT INTO tasksTABLE (name_task, status, local_date, local_time) VALUES ('Почитать почту', 'в процессе', CURRENT_DATE, CURRENT_TIME)
+INSERT INTO tasksTABLE (name_task, status, local_date, local_time) VALUES ('Встреча в 11:00', 'начать', CURRENT_DATE, CURRENT_TIME)
+INSERT INTO tasksTABLE (name_task, status, local_date, local_time) VALUES ('Подготовиться к выступлению', 'начать', CURRENT_DATE, CURRENT_TIME)
